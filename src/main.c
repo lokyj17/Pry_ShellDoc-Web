250cc76150ee8d95c933b84bbdb02557bfc0415b
lokyj17 <alanSjami@hotmail.com>
2026-06-01 01:57:35 +0000

    Para que se pueda abrir cualquier archivo ubicado en data

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "archivos.h"
 #include "procesos.h"
 #include "hilos.h"
@@ -8,23 +9,31 @@
 int main(int argc, char *argv[]) {
     printf("\nSISTEMA INTEGRADO SHELLDOC WEB - GRUPO 2\n\n");
 
-    // Definición de rutas relativas de trabajo
-    const char *ruta_sesion   = "../data/sesion_linux.txt";
+    if (argc < 2) {
+        fprintf(stderr, "ERROR: Debes especificar el nombre del archivo ubicado en data.\n");
+        fprintf(stderr, "Uso correcto: %s <nombre_archivo.txt>\n", argv[0]);
+        fprintf(stderr, "Ejemplo: %s sesion_linux.txt\n\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char ruta_sesion[512];
+    snprintf(ruta_sesion, sizeof(ruta_sesion), "../data/%s", argv[1]);
+
     const char *ruta_template = "../quarto/template.qmd";
     const char *ruta_salida   = "../quarto/informacion.qmd";
     
     const char *titulo_doc    = "Reporte Automatizado de Sesión Linux";
     const char *autor_doc     = "Grupo 2 - EPN";
 
-    // 1. Ejecutar Fase 1, 2 y 3 (Lectura, Hilos y tus Procesos Fork)
-    printf("Iniciando Procesamiento Base...\n");
+    printf("\nLeyendo archivo desde: %s\n\n", ruta_sesion);
+
+    printf("\nIniciando Procesamiento Base...\n");
     int res_base = procesarArchivoSesion(ruta_sesion);
     if (!res_base) {
         fprintf(stderr, "ERROR: Falla crítica en el procesamiento base.\n");
         return EXIT_FAILURE;
     }
 
-    // 2. Ejecutar Fase 4 (Generador de plantilla .qmd de Juan Jácome)
     printf("\nIniciando Fase 4 (Generación de Documento .qmd)...\n");
     int res_fase4 = generarDocumentoQmd(ruta_sesion, ruta_template, ruta_salida, titulo_doc, autor_doc);
     if (!res_fase4) {
@@ -32,12 +41,11 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    // 3. Ejecutar Fase 5 (Tu rol: Automatizar el renderizado a HTML usando system)
     printf("\nIniciando Fase 5 (Renderizado de Quarto a HTML)...\n");
     int resultado_render = system("quarto render ../quarto/informacion.qmd --to html");
 
     if (resultado_render == 0) {
-        printf("\n¡PIPELINE EJECUTADO CON ÉXITO!\n");
+        printf("\n¡EJECUTADO CON ÉXITO!\n");
         printf("Documento web generado en: ../quarto/informacion.html\n");
     } else {
         fprintf(stderr, "\nEl programa corrió, pero Quarto falló al renderizar.\n");
